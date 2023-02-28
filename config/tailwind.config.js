ed1757f88042dd7089488031d4e792a44725745c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-02-28 16:09:20 -0300

    add styles to home

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,9 +8,18 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        serif: ['Merriweather', 'serif'],
       },
     },
   },

3b3e4dd86e0aa961e4f98ae15d06de9a0383b4d1
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-03-01 16:53:16 -0300

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
+before_action :update_allowed_parameters, if: :devise_controller?
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|

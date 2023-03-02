06764af7e8861d7b8660f836b269f1427bb90780
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-03-02 09:54:33 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   
 get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/moon', to: 'application#moon', as: 'moon'
+  get '/sun', to: 'application#sun', as: 'sun'
   # Defines the root path route ("/")
   root "home#index"
 end

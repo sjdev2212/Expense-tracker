b90503a126380ac1a3e4f20046283521499b9c67
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-03-02 09:53:57 -0300

    add function to store cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ before_action :update_allowed_parameters, if: :devise_controller?
       u.permit(:name, :email, :password, :password_confirmation)
     end
   end
+
+  def moon
+    cookies[:moon] = {
+      value: 'dark mode on'
+    }
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def sun
+    cookies.delete(:moon)
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    else
+      redirect_to   root_path
+    end
+  end
 end

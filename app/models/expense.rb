1d2e2c546a38741af6ff92db230623e05e549170
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-03-07 18:47:00 -0300

    add expense model

---
+++ app/models/expense.rb
@@ -0,0 +1,7 @@
+class Expense < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: { message: 'This field can not be empty' }
+  validates :amount,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'The input should be greater or equal to 0' }
+end

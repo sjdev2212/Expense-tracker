1be9754cee24f2d1634db0b6b62a79cf6ab5f6f2
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-03-14 11:02:21 -0300

    styles

--- db/migrate/20230313113449_update.rb
+++ db/migrate/20230313113449_update.rb
@@ -1,5 +1,5 @@
 class Update < ActiveRecord::Migration[7.0]
   def change
-    change_column :expenses , :amount, :decimal, precision: 8, scale: 2
+    change_column :expenses , :amount, :decimal, precision: 8, scale: 3
   end
 end
